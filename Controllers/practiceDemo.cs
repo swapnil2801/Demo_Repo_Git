cef17115c4ae4d45cb073fbf40550d79579c0ffd
Swapnil Patil <sbpatil2801@gmail.com>
2024-04-16 17:01:57 +0530

    added New data

---
+++ Controllers/practiceDemo.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PracticeApp.Controllers
+{
+    public class practiceDemo : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
